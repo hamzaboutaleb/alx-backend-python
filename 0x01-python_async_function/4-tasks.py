c09656171fc8040742a59c46221d86834dfb6921
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-12-12 15:02:49 +0100

    task 4

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -4,7 +4,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int):
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Run the wait_random coroutine 'n' times with the specified 'max_delay'.
     Args:
