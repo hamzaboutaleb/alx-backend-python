3ec0d2c055e6100e9de3e50dd6650a1fbc925f29
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-12-12 14:51:14 +0100

    task 3

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -3,7 +3,7 @@ import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-async def wait_n(n: int, max_delay: int) -> List[asyncio.Task]:
+def wait_n(n: int, max_delay: int) -> List[asyncio.Task]:
     """
     Run the wait_random coroutine 'n' times with the specified 'max_delay'.
     Args:
