3ba7578f66b59f94b5cc92c080a02e1521087c3c
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-12-13 20:19:30 +0100

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""float to string"""
+
+
+def to_str(n: float) -> str:
+    """to string
+    Args:
+        n: number
+    Returns:
+        string
+    """
+    return str(n)
