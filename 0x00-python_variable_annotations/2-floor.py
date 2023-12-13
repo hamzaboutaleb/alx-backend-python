4d552c774bb26be24a501526149f9123e6defbe9
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-12-13 20:16:13 +0100

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""floor number"""
+import math
+
+
+def floor(n: float) -> int:
+    """ floor number
+    Args:
+        n: number to floor
+    Returns:
+        number floored
+    """
+
+    return math.floor(n)
