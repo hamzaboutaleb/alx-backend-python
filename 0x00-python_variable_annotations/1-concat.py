b0f51816d5ac16019cd48236572e986c340192df
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-12-13 20:13:14 +0100

    task 0

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""concat two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ concatinate two strings
+    Args:
+        str1: first string
+        str2: second string
+    Returns:
+        string
+    """
+    return f"{str1} {str2}"
