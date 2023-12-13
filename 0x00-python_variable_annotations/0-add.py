a99994fdfec72cda080f70ab6728b4cc610e93b7
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-12-13 20:09:08 +0100

    task 0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,4 +2,11 @@
 """ sum float number """
 
 def add(a: float, b: float) -> float:
+  """ add to float number
+    Args:
+      a: first float number
+      b: second float number
+    Returns:
+      sum of both numbers
+  """
   return a + b
